170e3d2af25afc7479eade899a423f0f2b7471bc
ffe <fatfingererr@gmail.com>
2017-12-26 23:00:35 +0800

    init commit

--- resources/templates/md/pages/about.md
+++
@@ -1,6 +0,0 @@
-{:title "About"
- :layout :page
- :page-index 0
- :navbar? true}
-
-Twitter : [@fatfingererr](https://twitter.com/fatfingererr)
--- scripts/deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-cd /d "..\docs"
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-xcopy /e /i /v /h /k "..\resources\public" ".\"
-cd /d ".."
-git add -A
-git commit -m "Update blog"
-git push origin master --force
