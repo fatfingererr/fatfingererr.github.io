578e305fabb4aa7b6d00fd41a5198de780e1cbdb
ffe <fatfingererr@gmail.com>
2017-12-26 22:59:24 +0800

    Update blog

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d %~dp0
-git checkout gh-pages --  "..\resources\public"
+git checkout gh-pages -- "..\resources\public"
+git checkout master
 cd /d ".."
 git add -A
 git commit -m "Update blog"
