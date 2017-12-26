b5b2685abc309239289ee578ccdece70b3730b11
ffe <fatfingererr@gmail.com>
2017-12-26 22:53:57 +0800

    Update blog

---
+++ scripts/deploy.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d %~dp0
+cd /d "..\docs"
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+xcopy /e /i /v /h /k "..\resources\public" ".\"
+cd /d ".."
+git add -A
+git commit -m "Update blog"
+git push origin master --force
